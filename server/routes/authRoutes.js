6ed246a4a86e074eca97c5688216b34ef8daef93
Greeshma Reddy <saigreeshma737@gmail.com>
2026-05-07 12:08:55 +0530

    Fixed auth routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-const User = require("../models/User");
 
 const router = express.Router();
 
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
 // SIGNUP
+
 router.post("/signup", async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password } = req.body;
 
-    // checking existing user
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
@@ -20,36 +20,39 @@ router.post("/signup", async (req, res) => {
       });
     }
 
-    // hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // create new user
     const newUser = new User({
       name,
+
       email,
+
       password: hashedPassword,
-      role,
     });
 
     await newUser.save();
 
     res.status(201).json({
-      message: "User registered successfully",
+      message: "Signup successful",
     });
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
-      message: error.message,
+      message: "Server error",
     });
   }
 });
 
 // LOGIN
+
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // find user
-    const user = await User.findOne({ email });
+    const user = await User.findOne({
+      email,
+    });
 
     if (!user) {
       return res.status(400).json({
@@ -57,7 +60,6 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    // compare passwords
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
@@ -66,25 +68,14 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    // create token
-    const token = jwt.sign(
-      {
-        id: user._id,
-        role: user.role,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "1d",
-      },
-    );
-
     res.status(200).json({
       message: "Login successful",
-      token,
     });
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
-      message: error.message,
+      message: "Server error",
     });
   }
 });
