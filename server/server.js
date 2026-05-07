98e966b3c138457e6c33e2a3812537b0ee0bfbbb
Greeshma Reddy <saigreeshma737@gmail.com>
2026-05-07 12:42:35 +0530

    Fixed Railway frontend serving

--- server/server.js
+++ server/server.js
@@ -1,58 +1,20 @@
 const express = require("express");
-
-const mongoose = require("mongoose");
-
 const cors = require("cors");
-
-const dotenv = require("dotenv");
-
 const path = require("path");
-
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
-// SERVE FRONTEND
 
 app.use(express.static(path.join(__dirname, "../client")));
 
-// ROUTES
-
-const authRoutes = require("./routes/authRoutes");
-
-const taskRoutes = require("./routes/taskRoutes");
-
-app.use("/api/auth", authRoutes);
-
-app.use("/api/tasks", taskRoutes);
-
-// ROOT ROUTE
-
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/login.html"));
 });
-
-// MONGODB
-
-mongoose
-  .connect(process.env.MONGO_URI)
-
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-
-  .catch((err) => {
-    console.log(err);
-  });
-
-// PORT
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
